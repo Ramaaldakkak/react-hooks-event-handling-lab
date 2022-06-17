6540c25a8db402ca42f701ce9a59faf070e4ab1c
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-06-17 16:50:11 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
 // Code EyesOnMe Component Here
+
+// create a EyesOnMe React component.
+
+// render a button with the text 'Eyes on me'.
+
+// On that button, add event handlers that listen for the focus and blur events.
+
+// When the focus event fires, use console.log to print out the text 'Good!'.
+
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
+
+
+export default function EyesOnMe() {
+  const eventHandlerBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  const eventHandlerFocus = () => {
+    console.log('Good!');
+  }
+
+  return(
+
+    <button
+    onFocus={eventHandlerFocus}
+    onBlur={eventHandlerBlur}>Eyes on me</button>
+
+  )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,25 @@
 // Code Keypad Component Here
+
+// create a Keypad React component.
+
+// In that component, render an input with the right type.
+
+// On that input, add an event handler that listens for the change event. => onChange
+
+// When that event fires, use console.log to print out the text 'Entering password...'.
+
+export default function Keypad() {
+
+  const eventHandlerChange = () => {
+    console.log('Entering password...');
+  }
+
+  return(
+
+    <input
+    placeholder="Enter password"
+    type="password"
+    onChange={eventHandlerChange}></input>
+
+  )
+}
